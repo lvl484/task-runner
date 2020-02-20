30eee9f80dee48b1522c97b825936c6d8eadec3a
Bohdan Volykh <wbodjaw@gmail.com>
2020-02-20 15:09:36 +0200

    add comments to methods

--- database/memory.go
+++ database/memory.go
@@ -3,9 +3,10 @@ package database
 import (
 	"context"
 	"fmt"
+	"sync"
+
 	"github.com/google/uuid"
 	"github.com/lvl484/task-runner/model"
-	"sync"
 )
 
 type Memory struct {
@@ -20,6 +21,8 @@ func NewMemory() *Memory {
 	}
 }
 
+// CreateTask receive input task, generate new UUID for it.
+// Set that task to map with all tasks and return task ID
 func (m *Memory) CreateTask(ctx context.Context, task *model.Task) (string, error) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -30,6 +33,7 @@ func (m *Memory) CreateTask(ctx context.Context, task *model.Task) (string, erro
 	return task.ID, nil
 }
 
+// DeleteTask receive task ID. Do deleting required task from map by ID
 func (m *Memory) DeleteTask(ctx context.Context, id string) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -38,6 +42,8 @@ func (m *Memory) DeleteTask(ctx context.Context, id string) error {
 	return nil
 }
 
+// UpdateTask receive task ID, which need to update,
+// and task, which must replace the selected by ID task
 func (m *Memory) UpdateTask(ctx context.Context, id string, task *model.Task) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -47,6 +53,8 @@ func (m *Memory) UpdateTask(ctx context.Context, id string, task *model.Task) er
 	return nil
 }
 
+// GetTask receive task ID, which need to get.
+// If selected task exists return task or error if task not found
 func (m *Memory) GetTask(ctx context.Context, id string) (*model.Task, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
