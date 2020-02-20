a0e9d68984014da04173fb8d85737b1a53f2caf1
Bohdan Volykh <wbodjaw@gmail.com>
2020-02-20 13:43:21 +0200

    sort imports

--- cmd/task-runner/main.go
+++ cmd/task-runner/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"log"
+
 	"github.com/lvl484/task-runner/config"
 	"github.com/lvl484/task-runner/database"
 	"github.com/lvl484/task-runner/runner"
 	"github.com/lvl484/task-runner/scheduler"
 	"github.com/lvl484/task-runner/server"
 	"github.com/lvl484/task-runner/service"
-	"log"
 )
 
 func main() {
