9663f5ba11d8ed003510708621bc47b4c69ff7f7
Bohdan Volykh <wbodjaw@gmail.com>
2020-02-20 13:42:11 +0200

    add consts ro Predefined actions

--- actions/select_actions.go
+++ actions/select_actions.go
@@ -2,26 +2,31 @@ package actions
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/lvl484/task-runner/model"
 )
 
-// Select predefined action
+const (
+	PDCurrentTime = "CurrentTime"
+	PDCurrentOS = "CurrentOS"
+	PDCurrentCPU = "CurrentCPU"
+)
+
+// SelectActions check json field task.Script. Compare it with all Predefined actions.
+// After that run the matched action and return result of its execution or error if action not found
 func SelectActions(ctx context.Context, task *model.Task) (string, error) {
 	action := task.Script
 	switch action {
-	case "CurrentTime":
-		result := CurrentTime()
-		return result, nil
-	case "CurrentOS":
-		result := CurrentOS()
-		return result, nil
-	case "CurrentCPU":
-		result := CurrentCPU()
-		return result, nil
+	case PDCurrentTime:
+		return CurrentTime(), nil
+	case PDCurrentOS:
+		return CurrentOS(), nil
+	case PDCurrentCPU:
+		return CurrentCPU(), nil
 	default:
-		fmt.Println("Undefined action")
-		return "Undefined action", nil
+		fmt.Println("\r\nUndefined action")
+		return "", errors.New("undefined action")
 	}
 }
